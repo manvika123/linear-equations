4492a30554a8285d5b488dcda44b9e85f62db4d7
manvika123 <manvikajaiswal1995@gmail.com>
2017-09-17 13:28:40 +0530

    first commit

---
+++ src/equa/equation.java
@@ -0,0 +1,14 @@
+package equa;
+
+public class equation {
+
+    public static void main(String[] args) {
+
+        double x,y;
+        x= ((50.2*-5.9)-(.55*-44.5))/((3.4*.55)-(2.1*50.2));
+
+       y= ((-44.5*2.1)-(-5.9*3.4))/((3.4*.55)-(2.1*50.2));
+
+        System.out.println("x and y are "+x +" "+y);
+}
+}
